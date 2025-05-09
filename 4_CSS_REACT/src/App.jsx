fa2b71dee2da646ea5cae32897a907b15b1646df
adrianolopees <adrianodeev1@gmail.com>
2025-05-09 11:20:06 -0300

    css inline dinamic

--- 4_CSS_REACT/src/App.jsx
+++ 4_CSS_REACT/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 20;
   return (
     <>
       <div className="App">
@@ -11,6 +12,22 @@ function App() {
         <MyComponent />
         <p>Esse é o parágraco e do App.jsx</p>
         <p className="my-comp-paragraph">Esse foi feito com classe</p>
+        {/* INLINE CSS */}
+        <p
+          style={{
+            color: "blue",
+            fontSize: "20px",
+            borderTop: "2px solid black",
+          }}
+        >
+          Esse foi feito com inline CSS Esse elemento foi estilizado de forma
+          inline
+        </p>
+
+        {/* CSS INLINE DINAMICO*/}
+        <h2 style={n < 21 ? { color: "purple" } : { color: "yellow" }}>
+          CSS dinâmico
+        </h2>
       </div>
     </>
   );
